d3b6bd2be19c5fccf187c46118d78b2dd66ad4d2
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-10 20:51:58 +0300

    initial commit

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,21 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, onToggleInStock }) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard
+          key={plant.id}
+          id={plant.id}
+          name={plant.name}
+          image={plant.image}
+          price={plant.price}
+          inStock={plant.inStock} 
+          onToggleInStock={onToggleInStock}  
+        />
+      ))}
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(""); 
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data))
+      .catch((error) => console.error("Error fetching plants:", error));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    const plantToSend = {
+      ...newPlant,
+      price: newPlant.price.toString(),
+    };
+
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+      body: JSON.stringify(plantToSend),
+    })
+      .then((response) => response.json())
+      .then((savedPlant) => {
+        setPlants([...plants, savedPlant]);
+      })
+      .catch((error) => console.error("Error adding plant:", error));
+  }
+
+  function handleToggleInStock(id) {
+    setPlants((prevPlants) =>
+      prevPlants.map((plant) =>
+        plant.id === id ? { ...plant, inStock: !plant.inStock } : plant
+      )
+    );
+  }
+
+  
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearchChange={setSearchTerm} /> 
+      <PlantList plants={filteredPlants} onToggleInStock={handleToggleInStock} />
     </main>
   );
 }
